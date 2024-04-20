18d1b9f40b7e1b9905b282486bf2a75cbd352636
Ulugbek Raxmankulov <mylogin.ulugbek@gmail.com>
2024-04-20 13:59:06 +0500

    starting

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/ulugbek0217/s21-info-bot
+
+go 1.22.2
+
+require github.com/go-telegram/bot v1.2.1 // indirect
---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/go-telegram/bot"
+	"github.com/go-telegram/bot/models"
+)
+
+func main() {
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	opts := []bot.Option{
+		bot.WithDefaultHandler(handler),
+	}
+
+	b, err := bot.New("5559681653:AAFbKDgbqpUHoycIhGEhoYs10uvCEDHtkZg", opts...)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	b.Start(ctx)
+}
+
+func handler(ctx context.Context, b *bot.Bot, update *models.Update) {
+	b.SendMessage(ctx, &bot.SendMessageParams{
+		ChatID: update.Message.Chat.ID,
+		Text:   update.Message.Text,
+	})
+}
